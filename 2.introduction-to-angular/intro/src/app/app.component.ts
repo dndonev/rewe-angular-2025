0c3ec9e610127fc78b768c5bee977613f7008579
Dobrin <dobrindonev@gmail.com>
2023-04-28 14:52:51 +0300

    [REWE] 2.Introduction-to-angular

---
+++ 2.introduction-to-angular/intro/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Todo App';
+  placeholder = 'Enter your name';
+  buttonText = 'Click me';
+
+  onButtonClick() {
+    this.title = 'Hello World';
+  };
+}
---
+++ 2.introduction-to-angular/intro/src/components/templates/templates.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-templates',
+  template: `
+    <h1>Templates</h1>
+    <input placeholder="template input">
+  `,
+  // templateUrl: './templates.component.html',
+  styleUrls: ['./templates.component.scss']
+})
+export class TemplatesComponent {
+
+}
