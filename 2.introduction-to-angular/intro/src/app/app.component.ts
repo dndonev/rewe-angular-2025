c8ffb03e314b20a69d3a333e082766a1dd5c490e
Dobrin <dobrindonev@gmail.com>
2023-04-28 14:53:16 +0300

    [REWE] 2.Introduction-to-angular

--- 2.introduction-to-angular/intro/src/app/app.component.ts
+++ 2.introduction-to-angular/intro/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todo App';
-  placeholder = 'Enter your name';
+  placeholder = 'Enter your todo';
   buttonText = 'Click me';
 
   onButtonClick() {
