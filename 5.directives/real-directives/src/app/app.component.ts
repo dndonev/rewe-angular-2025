8f5fb93303a3860fefc4fceb82a64ae945d1aeb1
Dobrin <dobrindonev@gmail.com>
2023-05-16 12:52:45 +0300

    5. Angular directives

--- 5.directives/real-directives/src/app/app.component.ts
+++ 5.directives/real-directives/src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'real-directives';
-  directiveBackgroundColor: string = 'lightpink';
+  directiveBackgroundColor: string = 'pink';
 }
